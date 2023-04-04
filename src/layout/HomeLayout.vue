ce6e38fe78f562abdc9318f6fe0af100d4c12090
Alexis Gilberth Renteria <gilberthr10@gmail.com>
2023-04-04 16:35:33 -0500

    ADD: Keep alive pages

--- src/layout/HomeLayout.vue
+++ src/layout/HomeLayout.vue
@@ -7,7 +7,11 @@ import { RouterView } from "vue-router";
   <TabNavigation />
   <main class="p-6">
     <router-view v-slot="{ Component }">
-      <component :is="Component" />
+      <transition>
+        <keep-alive>
+          <component :is="Component" />
+        </keep-alive>
+      </transition>
     </router-view>
   </main>
 </template>
